0d7604d198ba9754273bb2cc619b3d38d3af8406
Твоё Имя <@example.com>
2026-03-27 22:38:52 +0500

    fix

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('api/',include("user.urls")),
     path('api/',include("course.urls")),
     path('api/',include("homework.urls")),
     path('api/',include("lesson.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
